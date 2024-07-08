a7d2ce3f63cde0e24dfd45587f39e55c183b8cd6
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-07-08 11:05:22 +0530

    Initial uploads

---
+++ src/Products.js
@@ -0,0 +1,45 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "1 Car",
+    price: 2000,
+    imageUrl:
+      "./images/test.png",
+  },
+
+  {
+    id: 2,
+    title: "2 Car",
+    price: 300,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 3,
+    title: "3 Car",
+    price: 300,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 4,
+    title: "4 Car",
+    price: 95,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 5,
+    title: "5 Car",
+    price: 3500,
+    imageUrl:
+      "./images/test.png",
+  },
+
+  {
+    id: 6,
+    title: "5 Car",
+    price: 12,
+    imageUrl: "./images/test.png",
+  },
+];
