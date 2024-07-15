375c9635e0cae0b8082f31dce140df152d994d70
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-07-15 12:42:47 +0530

    Done

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -9,14 +9,61 @@ export const productsData = [
   ✔ • Working Dials [Holographic]<br>
   ✔ • Working Lights<br>
   ✔ • Custom Golden Rims<br>
-  ✔ • 4 Doors 4 Seats<br>
+  ✔ • 4 Doors 4export const productsData = [
+    {
+      id: 1,
+      title: "BMW M4 CS Custom 3 Seat Adjustments",
+      price: 2000,
+      description: ``,
+      imageUrl: "./images/test.png",
+      images: [
+        "./images/test.png",
+      ]
+    },
+  
+    {
+      id: 2,
+      title: "2 Car",
+      price: 300,
+      imageUrl: "./images/test.png",
+    },
+  
+    {
+      id: 3,
+      title: "3 Car",
+      price: 300,
+      imageUrl: "./images/test.png",
+    },
+  
+    {
+      id: 4,
+      title: "4 Car",
+      price: 95,
+      imageUrl: "./images/test.png",
+    },
+  
+    {
+      id: 5,
+      title: "5 Car",
+      price: 3500,
+      imageUrl:
+        "./images/test.png",
+    },
+  
+    {
+      id: 6,
+      title: "5 Car",
+      price: 12,
+      imageUrl: "./images/test.png",
+    },
+  ];
+   Seats<br>
   ✔ • Custom Engine Sound<br>
   ✔ • Auto Spoiler<br>
   ✔ • Fully Debadged [Removed Logos/Manufacturing]<br>
       Quality Interior`,
-      imageUrl: "https://github.com/Vibhav1207/FroDy-Customs/blob/main/images/goldenpalette.png",
       images: [
-        "./images/test.png",
+        "https://github.com/Vibhav1207/FroDy-Customs/blob/main/images/goldenpalette.png",
       ]
     },
   
