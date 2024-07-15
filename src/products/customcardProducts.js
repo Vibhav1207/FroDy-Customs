45eb8c5798ba6d6bf6b4ed71a7bf0dc9b6a9fb2d
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-07-15 12:18:06 +0530

    Update customcardProducts.js

--- src/products/customcardProducts.js
+++ src/products/customcardProducts.js
@@ -3,7 +3,17 @@ export const productsData = [
       id: 1,
       title: "BMW M4 CS Custom 3 Seat Adjustments",
       price: 2000,
-      description: ``,
+      description: `✨Audi RS7 Golden Palette✨<br>
+  ✔ • Custom Nameplate [Changeable]<br>
+  ✔ • Custom Realistic Handling [Included]<br>
+  ✔ • Working Dials [Holographic]<br>
+  ✔ • Working Lights<br>
+  ✔ • Custom Golden Rims<br>
+  ✔ • 4 Doors 4 Seats<br>
+  ✔ • Custom Engine Sound<br>
+  ✔ • Auto Spoiler<br>
+  ✔ • Fully Debadged [Removed Logos/Manufacturing]<br>
+  ✔ • Quality Interior,`
       imageUrl: "./images/test.png",
       images: [
         "./images/test.png",
