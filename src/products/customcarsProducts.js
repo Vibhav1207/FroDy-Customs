4482279518b80bd68125ad3e6cb8c4ad32bb09e4
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-07-16 11:11:22 +0530

    Bug fixing

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -1,58 +1,56 @@
 export const productsData = [
-    {
-      id: 1,
-      title: "Audi RS7 Golden Palette",
-      price: 12$,
-      description: `✨Debadged✨<br>
-  ✔ • Custom Nameplate [Changeable]<br>
-  ✔ • Custom Realistic Handling [Included]<br>
-  ✔ • Working Dials [Holographic]<br>
-  ✔ • Working Lights<br>
-  ✔ • Custom Golden Rims<br>
-  ✔ • 4 Doors 4 Seats<br>
-  ✔ • Custom Engine Sound<br>
-  ✔ • Auto Spoiler<br>
-      Quality Interior`,
-      imageUrl: "https://github.com/Vibhav1207/FroDy-Customs/blob/main/images/goldenpalette.png",
-      images: [
-        "./images/test.png",
-      ]
-    },
-  
-    {
-      id: 2,
-      title: "2 Car",
-      price: 300,
-      imageUrl: "./images/test.png",
-    },
-  
-    {
-      id: 3,
-      title: "3 Car",
-      price: 300,
-      imageUrl: "./images/test.png",
-    },
-  
-    {
-      id: 4,
-      title: "4 Car",
-      price: 95,
-      imageUrl: "./images/test.png",
-    },
-  
-    {
-      id: 5,
-      title: "5 Car",
-      price: 3500,
-      imageUrl:
-        "./images/test.png",
-    },
-  
-    {
-      id: 6,
-      title: "5 Car",
-      price: 12,
-      imageUrl: "./images/test.png",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "BMW M4 CS Custom 3 Seat Adjustments",
+    price: 2000,
+    description: `✨TYOEST✨<br>
+  ✔ • Primary Color - Changing Primary Color & Animated Grills<br>
+  ✔ • Secondary Color - Changing Hub & Bonnet Animated Strips<br>
+  ✔ • Wheel Color - Changeable<br>
+  ✔ • 3 Seats 2 Doors - [2 Front Seat, 1 on Roof]<br>
+  ✔ • Quality with Graphics<br>
+       Speaker Deck in Boot<br>
+       Realistic Handling<br>
+       Fully Debadged`,
+    imageUrl: "https://github.com/Vibhav1207/FroDy-Customs/blob/main/images/goldenpalette.png",
+    images: [
+      "./images/test.png",
+    ]
+  },
+
+  {
+    id: 2,
+    title: "2 Car",
+    price: 300,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 3,
+    title: "3 Car",
+    price: 300,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 4,
+    title: "4 Car",
+    price: 95,
+    imageUrl: "./images/test.png",
+  },
+
+  {
+    id: 5,
+    title: "5 Car",
+    price: 3500,
+    imageUrl:
+      "./images/test.png",
+  },
+
+  {
+    id: 6,
+    title: "5 Car",
+    price: 12,
+    imageUrl: "./images/test.png",
+  },
+];
