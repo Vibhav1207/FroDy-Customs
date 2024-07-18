e1893b42a59eb9444fa4e25cfebd73b14f8e1ec2
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-07-18 12:20:16 +0530

    Update customcarsProducts.js

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -3,7 +3,19 @@ export const productsData = [
     id: 1,
     title: "BMW M4 CS Custom 3 Seat Adjustments",
     price: 15$,
+    description: `✨Features✨<br>
+  ✔ • Primary Color - Changing Primary Color & Animated Grills<br>
+  ✔ • Secondary Color - Changing Hub & Bonnet Animated Strips<br>
+  ✔ • Wheel Color - Changeable<br>
+  ✔ • 3 Seats 2 Doors - [2 Front Seat, 1 on Roof]<br>
+  ✔ • Quality with Graphics<br>
+       Speaker Deck in Boot<br>
+       Realistic Handling<br>
+       Fully Debadged`,
     imageUrl: "https://github.com/Vibhav1207/FroDy-Customs/blob/main/images/m4cs.png",
+    images: [
+      "./images/test.png",
+    ]
   },
 
   {
