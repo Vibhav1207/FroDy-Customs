c61b31198fa6baeadfde7d1679f981ec9212f701
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-07-22 16:22:14 +0530

    test

--- src/products/debadgedProducts.js
+++ src/products/debadgedProducts.js
@@ -51,4 +51,55 @@ export const productsData = [
     price: 10,
     imageUrl: "./images/debadged/ctsv.jpg",
   },
+
+  {
+    id: 8,
+    title: "BMW IX EV",
+    price: 10,
+    imageUrl: "./images/debadged/IXEV3.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Lexus LS600 5 Seat",
+    price: 10,
+    imageUrl:
+      "./images/debadged/lexus.png",
+  },
+
+  {
+    id: 10,
+    title: "Lexus LFA 2024",
+    price: 10,
+    imageUrl: "./images/debadged/lfa2024.jpg",
+  },
+
+  {
+    id: 11,
+    title: "BMW M5 Tour",
+    price: 10,
+    imageUrl: "./images/debadged/m5tour.jpg",
+  },
+
+  {
+    id: 12,
+    title: "BMW M135i",
+    price: 10,
+    imageUrl:
+      "./images/debadged/m135i4.png",
+  },
+
+  {
+    id: 13,
+    title: "BMW M330i Variant Pack-2",
+    price: 15,
+    imageUrl: "./images/debadged/m330ivariant.jpg",
+  },
+
+  {
+    id: 14,
+    title: "Chevrolette Mantaray 2024",
+    price: 10,
+    imageUrl: "./images/debadged/manray.jpg",
+  },
 ];
