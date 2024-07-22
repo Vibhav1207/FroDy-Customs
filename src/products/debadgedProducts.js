6b47d5171a4fe2affb124b9925f750960f9c4c68
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-07-22 16:32:06 +0530

    Update debadgedProducts.js

--- src/products/debadgedProducts.js
+++ src/products/debadgedProducts.js
@@ -2,7 +2,7 @@ export const productsData = [
   {
     id: 1,
     title: "BMW i7",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/i7.png",
     images: [
       "./images/debadged/i7.png",
@@ -12,28 +12,28 @@ export const productsData = [
   {
     id: 2,
     title: "Subaru BRZ",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/brz.jpg",
   },
 
   {
     id: 3,
     title: "Dodge Charger",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/charger.jpg",
   },
 
   {
     id: 4,
     title: "Dodge Charger Bodykit",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/chargerbodykit.jpg",
   },
 
   {
     id: 5,
     title: "Mercedes AMG CLS63",
-    price: 10,
+    price: 12,
     imageUrl:
       "./images/debadged/cls63.png",
   },
@@ -41,28 +41,28 @@ export const productsData = [
   {
     id: 6,
     title: "Lamborghini Countach",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/countach.jpg",
   },
 
   {
     id: 7,
     title: "Cadillac CTSV",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/ctsv.jpg",
   },
 
   {
     id: 8,
     title: "BMW IX EV",
-    price: 10,
+    price: 12,
     imageUrl: "./images/debadged/IXEV3.jpg",
   },
 
   {
     id: 9,
     title: "Lexus LS600 5 Seat",
-    price: 10,
+    price: 12,
     imageUrl:
       "./images/debadged/lexus.png",
   },
@@ -70,21 +70,21 @@ export const productsData = [
   {
     id: 10,
     title: "Lexus LFA 2024",
-    price: 10,
-    imageUrl: "./images/debadged/lfa2024.jpg",
+    price: 12,
+    imageUrl: "./images/debadged/lfa2024.png",
   },
 
   {
     id: 11,
     title: "BMW M5 Tour",
-    price: 10,
-    imageUrl: "./images/debadged/m5tour.jpg",
+    price: 12,
+    imageUrl: "./images/debadged/m5tour.png",
   },
 
   {
     id: 12,
     title: "BMW M135i",
-    price: 10,
+    price: 12,
     imageUrl:
       "./images/debadged/m135i4.png",
   },
@@ -99,7 +99,65 @@ export const productsData = [
   {
     id: 14,
     title: "Chevrolette Mantaray 2024",
-    price: 10,
+    price: 12,
+    imageUrl: "./images/debadged/manray.jpg",
+  },
+
+  {
+    id: 15,
+    title: "Toyota Supra Bodykit 5 Seat",
+    price: 12,
+    imageUrl: "./images/debadged/pupra.jpg",
+  },
+
+  {
+    id: 16,
+    title: "Audi R8 2024",
+    price: 12,
+    imageUrl: "./images/debadged/r8.png",
+  },
+
+  {
+    id: 17,
+    title: "Nissan GTR35 Custom WB",
+    price: 12,
+    imageUrl:
+      "./images/debadged/r35custom.jpg",
+  },
+
+  {
+    id: 18,
+    title: "Nissan GTR35 WB",
+    price: 12,
+    imageUrl: "./images/debadged/r35widebody.jpg",
+  },
+
+  {
+    id: 19,
+    title: "Nissan GTR34 Custom",
+    price: 12,
+    imageUrl: "./images/debadged/r341.jpg",
+  },
+
+  {
+    id: 20,
+    title: "Nissan GTR36 Custom",
+    price: 12,
+    imageUrl:
+      "./images/debadged/r367.png",
+  },
+
+  {
+    id: 21,
+    title: "Range Rover 2024 Custom",
+    price: 12,
+    imageUrl: "./images/debadged/rr20247.png",
+  },
+
+  {
+    id: 14,
+    title: "Chevrolette Mantaray 2024",
+    price: 12,
     imageUrl: "./images/debadged/manray.jpg",
   },
 ];
