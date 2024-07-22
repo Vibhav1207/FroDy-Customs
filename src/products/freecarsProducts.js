ef40bcf51555e413d6c761282883c2c6e07aeb3a
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-07-22 16:53:36 +0530

    Update freecarsProducts.js

--- src/products/freecarsProducts.js
+++ src/products/freecarsProducts.js
@@ -16,34 +16,5 @@ export const productsData = [
       price: 0,
       imageUrl: "./images/freecars/stang.png",
     },
-  
-    {
-      id: 3,
-      title: "3 Car",
-      price: 300,
-      imageUrl: "./images/test.png",
-    },
-  
-    {
-      id: 4,
-      title: "4 Car",
-      price: 95,
-      imageUrl: "./images/test.png",
-    },
-  
-    {
-      id: 5,
-      title: "5 Car",
-      price: 3500,
-      imageUrl:
-        "./images/test.png",
-    },
-  
-    {
-      id: 6,
-      title: "5 Car",
-      price: 12,
-      imageUrl: "./images/test.png",
-    },
   ];
   
