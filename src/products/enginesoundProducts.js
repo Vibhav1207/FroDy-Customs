6238100ce7aeb05cc218d1d232be20db92869124
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-07-25 19:45:07 +0530

    Added page for engine sound

---
+++ src/products/enginesoundProducts.js
@@ -0,0 +1,21 @@
+export const productsData = [
+    {
+      id: 1,
+      title: "Sound 1",
+      price: 0,
+      description: ``,
+      imageUrl: "./images",
+      images: [
+        "./images",
+      ]
+    },
+  
+    {
+      id: 2,
+      title: "Sound 2",
+      price: 0,
+      imageUrl: "./images",
+    },
+  
+  ];
+  
