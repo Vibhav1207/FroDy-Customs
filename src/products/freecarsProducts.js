237c1f3a849a0d6f1588c406fa034520159253ea
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-07-25 19:40:57 +0530

    Fixed image / products not loading

--- src/products/freecarsProducts.js
+++ src/products/freecarsProducts.js
@@ -1,20 +1,20 @@
 export const productsData = [
-    {
-      id: 1,
-      title: "Rolls Royce Ghost",
-      price: 0,
-      description: ``,
-      imageUrl: "./images/freecars/ghost.png",
-      images: [
-        "./images/freecars/ghost.png",
-      ]
-    },
-  
-    {
-      id: 2,
-      title: "Mustang GT500 Gold",
-      price: 0,
-      imageUrl: "./images/freecars/stang.png",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Rolls Royce Ghost",
+    price: 0,
+    description: ``,
+    imageUrl: "./images/freecars/ghost.png",
+    images: [
+      "./images/freecars/ghost.png",
+    ]
+  },
+
+  {
+    id: 2,
+    title: "Mustang GT500 Gold",
+    price: 0,
+    imageUrl: "./images/freecars/stang.png",
+  },
+
+];
