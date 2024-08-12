119327440a36d96266e1927ca3a075c17b5b2cac
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-08-12 06:53:47 +0530

    Update flyingcarsProducts.js

--- src/products/flyingcarsProducts.js
+++ src/products/flyingcarsProducts.js
@@ -39,12 +39,7 @@ export const productsData = [
       price: 16,
       imageUrl: "./images//flying/flyingdonk.jpg",
          paypalForm: `
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-  <input type="hidden" name="cmd" value="_s-xclick" />
-  <input type="hidden" name="hosted_button_id" value="3ANQMR7BW54LU" />
-  <input type="hidden" name="currency_code" value="USD" />
-  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
-</form> 
+ 
     `,
    productLink : "https://reefbot.tech"
     },
