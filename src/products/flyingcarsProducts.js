92c7621fc21c4cbaac0238556acf69880deeebc1
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-08-14 13:45:55 +0530

    Update flyingcarsProducts.js

--- src/products/flyingcarsProducts.js
+++ src/products/flyingcarsProducts.js
@@ -34,11 +34,16 @@ export const productsData = [
   
     {
       id: 3,
-      title: "Dodge Charger",
+      title: "Dodge Charger Fly",
       price: 16,
       imageUrl: "./images//flying/flyingcharger.jpg",
          paypalForm: `
- 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="AFDQKNE9WZ8Y6" />
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form>
     `,
    productLink : "https://reefbot.tech"
     },
@@ -49,7 +54,12 @@ export const productsData = [
       price: 16,
       imageUrl: "./images//flying/flyingdonk.jpg",
          paypalForm: `
- 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="PQENFJNG77E3C" />
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form>
     `,
    productLink : "https://reefbot.tech"
     },
