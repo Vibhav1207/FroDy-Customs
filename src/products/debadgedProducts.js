99491a9701c525a43743cdfb6cdf1c76b48d36cf
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-08-17 18:49:14 +0530

    Update debadgedProducts.js

--- src/products/debadgedProducts.js
+++ src/products/debadgedProducts.js
@@ -216,7 +216,12 @@ export const productsData = [
     price: 12,
     imageUrl: "./images/debadged/pupra.jpg",
        paypalForm: `
- 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="PXWWMZPG5N9SG" />
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form>
     `,
    productLink : "https://reefbot.tech"
   },
