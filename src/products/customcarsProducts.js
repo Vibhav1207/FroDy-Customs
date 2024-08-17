c0baec5d0bb9e23f8160db42eba5a3971b095e04
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-08-17 19:05:24 +0530

    Update customcarsProducts.js

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -65,23 +65,6 @@ export const productsData = [
 
   {
     id: 5,
-    title: "E46 Car Meetup",
-    price: 25,
-    imageUrl:
-      "./images/customcars/e46nfs.png",
-         paypalForm: `
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-  <input type="hidden" name="cmd" value="_s-xclick" />
-  <input type="hidden" name="hosted_button_id" value="5G4Q248XTU8LL" />
-  <input type="hidden" name="currency_code" value="USD" />
-  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
-</form>
-    `,
-   productLink : "https://reefbot.tech"
-  },
-
-  {
-    id: 6,
     title: "E60 Itachi",
     price: 20,
     imageUrl: "./images/customcars/e60itachi.png",
@@ -97,7 +80,7 @@ export const productsData = [
   },
 
   {
-    id: 7,
+    id: 6,
     title: "GT Demon",
     price: 20,
     imageUrl: "./images/customcars/gtdemon.png",
@@ -113,7 +96,7 @@ export const productsData = [
   },
 
   {
-    id: 8,
+    id: 7,
     title: "Golden RS7",
     price: 20,
     imageUrl: "./images/customcars/goldens.png",
@@ -129,7 +112,7 @@ export const productsData = [
 },
 
   {
-    id: 10,
+    id: 8,
     title: "Mercedes AMG G63 Custom",
     price: 35,
     imageUrl: "./images/customcars/g63custom.png",
@@ -145,7 +128,7 @@ export const productsData = [
   },
 
   {
-    id: 11,
+    id: 9,
     title: "Alfa Romeo Custom",
     price: 15,
     imageUrl: "./images/customcars/alfaromeo.png",
