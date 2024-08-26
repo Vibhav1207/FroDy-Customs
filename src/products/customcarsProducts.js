6e8f584bfbfb61652c78d7c3e2d5c8627b91656e
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-08-26 17:54:57 +0530

    Update customcarsProducts.js

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -158,5 +158,21 @@ export const productsData = [
     `,
    productLink : "https://reefbot.tech"
   },
+
+  {
+    id: 11,
+    title: "Pfister CometS2 Widebody",
+    price: 22,
+    imageUrl: "./images/customcars/pfistercomets2.png",
+       paypalForm: `
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="26VSKVBUW79LN" />
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form>
+    `,
+   productLink : "https://reefbot.tech"
+  },
 ];
 
