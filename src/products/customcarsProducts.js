fad75ef6d14b885420d8364237a8255a85d40b55
YoutubeMrFroDy <114962885+YoutubeMrFroDy@users.noreply.github.com>
2024-10-30 14:05:00 +0530

    t

--- src/products/customcarsProducts.js
+++ src/products/customcarsProducts.js
@@ -190,5 +190,21 @@ export const productsData = [
     `,
    productLink : "https://reefbot.tech"
   },
+
+  {
+    id: 12,
+    title: "Gold Edition Bentley Batur 2025",
+    price: 30,
+    imageUrl: "./images/customcars/batur.png",
+       paypalForm: `
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick" />
+  <input type="hidden" name="hosted_button_id" value="ELKDTSAYSHRXC" />
+  <input type="hidden" name="currency_code" value="USD" />
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+</form>
+    `,
+   productLink : "https://reefbot.tech"
+  },
 ];
 
